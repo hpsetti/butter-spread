58cb7abefc1599b47e5bee530ca20d94ab0b93ad
Harish Pillarisetti <44186210+hpsetti@users.noreply.github.com>
2022-10-27 01:49:04 +0530

    added build files

---
+++ src/views/HomeView.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="home">
+    <div class="home__header">
+      <h1>Site building in progress :)</h1>
+      <button>Dashboard</button>
+    </div>
+    <div>
+      <img
+        class="temp-illustration"
+        src="../assets/Illustrations/Sample-38.svg"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from "@/components/HelloWorld.vue";
+
+export default {
+  name: "HomeView",
+  components: {
+    // HelloWorld,
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.home__header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+.temp-illustration {
+  height: 100%;
+  width: 100%;
+}
+</style>
